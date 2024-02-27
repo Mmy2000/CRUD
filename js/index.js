0f625c3d3551dfdaeed6fc101ab833b8f9fdb81f
Mmy2000 <yahianada005@gmail.com>
2024-02-27 13:53:33 +0200

    adding some fatuers

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ function addProduct() {
     products.push(product);
     console.log(products);
     clear();
+    displayProduct();
 }
 function clear() {
     productName.value = "" 
@@ -21,4 +22,23 @@ function clear() {
     productCat.value = "" 
     productDesc.value = "" 
 
+}
+function displayProduct() {
+    var box = ''
+    for (let i = 0; i < products.length; i++) {
+        box += `<div class="card"  style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${products[i].id}</h5>
+                    <h5 class="card-title">${products[i].price}</h5>
+                    <h5 class="card-title">${products[i].category}</h5>
+                    <p class="card-text">${products[i].desc}</p>
+                    <a href="#" class="btn btn btn-outline-warning">Update</a>
+                    <a href="#" class="btn btn btn-outline-danger">Delete</a>
+                </div>
+            </div>`
+        
+        
+    }
+    document.getElementById("rowData").innerHTML = box
 }
